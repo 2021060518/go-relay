7947bc72149a689b3dc8ff00978e6c8789ed2d4d
2021060518 <104888345+2021060518@users.noreply.github.com>
2024-02-26 14:37:08 +0800

    add proxy

--- handler/mirror.go
+++ handler/mirror.go
@@ -5,6 +5,8 @@ import (
 	"go-relay/common"
 	"io"
 	"net/http"
+	"golang.org/x/net/proxy"
+	"os"
 )
 
 func MirrorHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,16 +16,33 @@ func MirrorHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	req.Header = r.Header.Clone()
-	client := &http.Client{}
+
+	// 创建一个代理地址的拨号器
+	dialer, err := proxy.SOCKS5("tcp", "172.17.0.5:1080", nil, proxy.Direct)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "can't connect to the proxy:", err)
+		os.Exit(1)
+	}
+
+	// 设置 http.Transport 结构体
+	httpTransport := &http.Transport{}
+
+	// 设置连接的代理
+	httpTransport.Dial = dialer.Dial
+
+	// 创建连接
+	client := &http.Client{Transport: httpTransport}
 
 	resp, err := client.Do(req)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadGateway)
 		return
 	}
+
 	for k, v := range resp.Header {
 		w.Header().Set(k, v[0])
 	}
+
 	w.WriteHeader(resp.StatusCode)
 	_, err = io.Copy(w, resp.Body)
 	if err != nil {
